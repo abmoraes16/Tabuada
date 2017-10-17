e309026729b90177a614fe5ba1cec8b40a794c71
FIC <a.bmoraes@outlook.com>
2017-10-17 20:33:51 -0200

    tabuada

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Tabuada
+{
+    class Program
+    {
+        static int valor = 0;
+
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o número para saber a tabuada: ");
+            valor = Int16.Parse(Console.ReadLine());
+
+            for(int i=0; i<=10; i++){
+                Console.WriteLine(valor +" * "+ i + " = " + (valor * i) );
+            }
+
+        }
+    }
+}
